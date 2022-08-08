eab72f0b41327d2adca4dc95c1f69204af621fcb
r1ck12 <rickregien@gmail.com>
2022-08-08 14:23:19 -0300

    remove theme switcher from Header and move wallet button

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource theme-ui */
 import Link from 'next/link'
-import { Button, Container, Flex, Text } from '@theme-ui/components'
+import { Button, Container, Flex, Text, Box } from '@theme-ui/components'
 
 import WalletManager from '@/components/WalletManager/WalletManager'
 import { useState } from 'react'
@@ -9,148 +9,120 @@ import { useColorMode } from 'theme-ui'
 
 const Header = () => {
 	const [isMobileMenuActive, setIsMobileMenuActive] = useState(false)
-	const [colorMode, setColorMode] = useColorMode()
 
 	return (
 		<Flex
 			sx={{
-				position: 'sticky',
+				position: 'absolute',
+				width: '100%',
 				top: 0,
 				zIndex: 9,
-				background: (theme) => theme.colors?.backgroundGradient,
-				borderBottom: '1px solid',
-				borderColor: 'background2',
+				background: 'transparent',
+				alignItems: 'flex-end',
+				justifyContent: 'flex-end',
+				maxWidth: '1600px',
 			}}
 		>
-			<Container>
+			<Flex
+				sx={{
+					alignItems: 'center',
+					justifyContent: 'flex-end',
+					padding: '1.6rem 1.6rem 0',
+					height: '8rem',
+				}}
+			>
 				<Flex
+					as='nav'
 					sx={{
+						gap: '1.6rem',
+						display: 'none',
 						alignItems: 'center',
-						justifyContent: 'space-between',
-						padding: '1.6rem 1.6rem 0',
-						height: '8rem',
+						alignSelf: 'stretch',
+
+						/** Mobile styles when the menu is active */
+						...(isMobileMenuActive && {
+							display: 'flex',
+							position: 'fixed',
+							flexDirection: 'column',
+							alignItems: 'center',
+							top: '0',
+							left: '0',
+							width: '100vw',
+							height: '100vh',
+							transition:
+								'opacity 0.125s cubic-bezier(0.175, 0.885, 0.32, 1.275),visibility 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275)',
+							backgroundColor: 'background',
+							zIndex: 99,
+
+							'& > a': {
+								marginBottom: '.8rem',
+							},
+
+							'&.active': {
+								visibility: 'visible',
+								opacity: 1,
+							},
+						}),
+
+						/** Desktop styles (reset mobile) */
+						'@media (min-width: 768px)': {
+							display: 'flex',
+							flexDirection: 'row',
+							padding: 0,
+							position: 'relative',
+						},
 					}}
 				>
-					<Link href='/' passHref>
-						<Flex as='a' sx={{ alignItems: 'center', flexDirection: 'column' }}>
-							<Flex sx={{ alignItems: 'center' }}>
-								<Text as='h1' variant='headingSpecial' ml='.4rem'>
-									Winner Wheel
-								</Text>
-							</Flex>
-						</Flex>
-					</Link>
-					<Text
-						variant='small'
+					<Flex
 						sx={{
-							marginRight: 'auto',
+							alignSelf: 'stretch',
+							justifyContent: 'flex-end',
+							borderBottom: '1px solid',
+							borderColor: 'background2',
+							padding: '1.6rem',
+							height: '8rem',
+							alignItems: 'center',
+							...(!isMobileMenuActive && { display: 'none' }),
 						}}
 					>
-						&nbsp;&nbsp;&nbsp;&#8226;&nbsp;
-						{process.env.NEXT_PUBLIC_CONNECTION_NETWORK}
-					</Text>
-
+						<Button
+							sx={{
+								padding: '.8rem',
+							}}
+							onClick={() => setIsMobileMenuActive(false)}
+						>
+							<CloseIcon />
+						</Button>
+					</Flex>
+					{/* Change theme-ui color mode */}
 					<Flex
-						as='nav'
 						sx={{
-							gap: '1.6rem',
-							display: 'none',
 							alignItems: 'center',
+							justifyContent: 'center',
 							alignSelf: 'stretch',
 
-							/** Mobile styles when the menu is active */
-							...(isMobileMenuActive && {
-								display: 'flex',
-								position: 'fixed',
-								flexDirection: 'column',
-								alignItems: 'center',
-								top: '0',
-								left: '0',
-								width: '100vw',
-								height: '100vh',
-								transition: 'opacity 0.125s cubic-bezier(0.175, 0.885, 0.32, 1.275),visibility 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275)',
-								backgroundColor: 'background',
-								zIndex: 99,
-
-								'& > a': {
-									marginBottom: '.8rem',
-								},
-
-								'&.active': {
-									visibility: 'visible',
-									opacity: 1,
-								},
-							}),
-
-							/** Desktop styles (reset mobile) */
-							'@media (min-width: 768px)': {
-								display: 'flex',
-								flexDirection: 'row',
-								padding: 0,
-								position: 'relative',
+							'&:hover': {
+								borderBottomWidth: '1px',
+								borderBottomStyle: 'solid',
+								borderBottomColor: 'text',
 							},
 						}}
-					>
-						<Flex
-							sx={{
-								alignSelf: 'stretch',
-								justifyContent: 'flex-end',
-								borderBottom: '1px solid',
-								borderColor: 'background2',
-								padding: '1.6rem',
-								height: '8rem',
-								alignItems: 'center',
-								...(!isMobileMenuActive && { display: 'none' }),
-							}}
-						>
-							<Button
-								sx={{
-									padding: '.8rem',
-								}}
-								onClick={() => setIsMobileMenuActive(false)}
-							>
-								<CloseIcon />
-							</Button>
-						</Flex>
-						{/* Change theme-ui color mode */}
-						<Flex
-							sx={{
-								alignItems: 'center',
-								justifyContent: 'center',
-								alignSelf: 'stretch',
-
-								'&:hover': {
-									borderBottomWidth: '1px',
-									borderBottomStyle: 'solid',
-									borderBottomColor: 'text',
-								},
-							}}
-						>
-							<Button
-								variant='resetted'
-								onClick={(e) => {
-									setColorMode(colorMode === 'default' ? 'light' : 'default')
-								}}
-							>
-								{colorMode === 'default' ? <SunLogo /> : <MoonLogo />}
-							</Button>
-						</Flex>
-						{/* Finish menu items */}
-						<WalletManager />
-					</Flex>
-					<Button
-						sx={{
-							padding: '.8rem',
-							'@media(min-width: 768px)': {
-								display: 'none',
-							},
-						}}
-						onClick={() => setIsMobileMenuActive(true)}
-					>
-						<MenuIcon />
-					</Button>
+					></Flex>
+					{/* Finish menu items */}
+					<WalletManager />
 				</Flex>
-			</Container>
+				<Button
+					sx={{
+						padding: '.8rem',
+						'@media(min-width: 768px)': {
+							display: 'none',
+						},
+					}}
+					onClick={() => setIsMobileMenuActive(true)}
+				>
+					<MenuIcon />
+				</Button>
+			</Flex>
 		</Flex>
 	)
 }
