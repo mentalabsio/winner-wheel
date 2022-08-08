91fafae52969c62d7700c76d48e0b5df1981129d
r1ck12 <rickregien@gmail.com>
2022-08-08 14:23:53 -0300

    remove roulette spin button, add api call

--- components/Roulette/Roulette.tsx
+++ components/Roulette/Roulette.tsx
@@ -1,6 +1,7 @@
+import { useWallet } from '@solana/wallet-adapter-react'
 import dynamic from 'next/dynamic'
 import { useEffect, useState } from 'react'
-import { Button } from 'theme-ui'
+import { Box, Button } from 'theme-ui'
 
 export interface StyleType {
 	backgroundColor?: string
@@ -35,46 +36,145 @@ const Wheel = dynamic<{
 })
 
 const data = [
-	{ option: '1' },
-	{ option: '2' },
-	{ option: '3' },
-	{ option: '4' },
-	{ option: '5' },
-	{ option: '6' },
-	{ option: '7' },
-	{ option: '8' },
-	{ option: '9' },
-	{ option: '10' },
-	{ option: '11' },
-	{ option: '12' },
+	{
+		option: '⟳',
+		style: {
+			backgroundColor: '#494646',
+			textColor: '#000',
+		},
+	},
+	{
+		option: '0',
+		style: {
+			backgroundColor: '#CF2424',
+			textColor: '#000',
+		},
+	},
+	{
+		option: '⟳',
+		style: {
+			backgroundColor: '#494646',
+			textColor: '#000',
+		},
+	},
+	{
+		option: '2X',
+		style: {
+			backgroundColor: '#E1CA39',
+			textColor: '#000',
+		},
+	},
+	{
+		option: '0',
+		style: {
+			backgroundColor: '#CF2424',
+			textColor: '#000',
+		},
+	},
+	{
+		option: '⟳',
+		style: {
+			backgroundColor: '#494646',
+			textColor: '#000',
+		},
+	},
+	{
+		option: '2X',
+		style: {
+			backgroundColor: '#E1CA39',
+			textColor: '#000',
+		},
+	},
+	{
+		option: '0',
+		style: {
+			backgroundColor: '#CF2424',
+			textColor: '#000',
+		},
+	},
+	{
+		option: '3X',
+		style: {
+			backgroundColor: '#A822AD',
+			textColor: '#000',
+		},
+	},
+	{
+		option: '0',
+		style: {
+			backgroundColor: '#CF2424',
+			textColor: '#000',
+		},
+	},
+	{
+		option: '2X',
+		style: {
+			backgroundColor: '#E1CA39',
+			textColor: '#000',
+		},
+	},
+	{
+		option: '0',
+		style: {
+			backgroundColor: '#CF2424',
+			textColor: '#000',
+		},
+	},
 ]
 
 export default () => {
 	const [mustSpin, setMustSpin] = useState(false)
 	const [prizeNumber, setPrizeNumber] = useState(0)
+	const { signTransaction } = useWallet()
 
 	console.log(mustSpin)
 
-	const handleStartSpinning = () => {
+	const handleStartSpinning = async () => {
 		console.log('started')
-		const newPrizeNumber = Math.floor(Math.random() * data.length)
-		setPrizeNumber(newPrizeNumber)
+		// const tx = new Transaction()
+
+		// transfer from user to house
+		// const ix = new Transfer({})
+
+		// const signed = signTransaction(tx)
+
+		// handleTransfer (API)
+		const newPrizeNumber = await (
+			await fetch('/api/transactions/getPrize', {
+				// body: signed.serialize(),
+				method: 'POST',
+				body: JSON.stringify({
+					dataLength: data.length,
+				}),
+			})
+		).json()
+
+		setPrizeNumber(newPrizeNumber.data)
 		setMustSpin(true)
+		console.log('winner index:', newPrizeNumber.data)
+		console.log('winner option:', data[newPrizeNumber.data])
 	}
 
 	useEffect(() => {}, [mustSpin])
 
 	return (
 		<>
-			<Wheel
-				mustStartSpinning={mustSpin}
-				prizeNumber={prizeNumber}
-				data={data}
-				onStopSpinning={() => {
-					setMustSpin(false)
+			<Box
+				sx={{
+					cursor: 'pointer',
+					marginBottom: '2rem',
 				}}
-			/>
-			<Button onClick={handleStartSpinning}>SPIN</Button>
+				onClick={handleStartSpinning}
+			>
+				<Wheel
+					mustStartSpinning={mustSpin}
+					prizeNumber={prizeNumber}
+					data={data}
+					onStopSpinning={() => {
+						setMustSpin(false)
+					}}
+				/>
+			</Box>
 		</>
 	)
 }
