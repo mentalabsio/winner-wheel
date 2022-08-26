2ce86631aae5d1af9a83a614905aa74ccc561f96
lucas <lucasfonsecard@icloud.com>
2022-08-25 22:04:14 -0300

    tests: add test for createBetProff handler

--- tests/winner-wheel.ts
+++ tests/winner-wheel.ts
@@ -1,14 +1,19 @@
 import * as anchor from "@project-serum/anchor";
-import { Keypair, Transaction, TransactionInstruction } from "@solana/web3.js";
+import {
+  Keypair,
+  Signer,
+  Transaction,
+  TransactionInstruction,
+} from "@solana/web3.js";
 import { expect } from "chai";
 import { WinnerWheelProgram } from "../app/lib";
-import { House } from "../app/lib/gen/accounts";
+import { BetProof, House } from "../app/lib/gen/accounts";
 import { fromTxError } from "../app/lib/gen/errors";
-import { findHouseAddress } from "../app/lib/pda";
+import { findBetProofAddress, findHouseAddress } from "../app/lib/pda";
 
 const send = async (
   provider: anchor.AnchorProvider,
-  signers: [],
+  signers: Signer[],
   ...ixs: TransactionInstruction[]
 ): Promise<string> => {
   try {
@@ -35,7 +40,7 @@ describe("winner-wheel", () => {
   anchor.setProvider(provider);
 
   const connection = provider.connection;
-  const program = WinnerWheelProgram();
+  const program = WinnerWheelProgram(connection);
 
   const authority = provider.wallet;
 
@@ -43,6 +48,12 @@ describe("winner-wheel", () => {
   const treasuryAccount = Keypair.generate();
   const feeVaultOne = Keypair.generate();
   const feeVaultTwo = Keypair.generate();
+
+  // House doesn't exist yet, but we can find it's address anyway.
+  const houseAddress = findHouseAddress({
+    id: 0,
+    authority: authority.publicKey,
+  });
 
   it("should be able to initialize a house", async () => {
     const ix = program.createHouseInstruction({
@@ -61,24 +72,42 @@ describe("winner-wheel", () => {
 
     await send(provider, [], ix);
 
-    const houseAddress = findHouseAddress({
-      id: 0,
-      authority: authority.publicKey,
-    });
     const houseAccount = await House.fetch(connection, houseAddress);
 
+    console.log(houseAccount.toJSON());
     expect(houseAccount.feeBasisPoints).to.equal(125);
   });
 
   it("should be able to create a bet proof", async () => {
+    const user = Keypair.generate();
+
+    await connection.confirmTransaction(
+      await connection.requestAirdrop(user.publicKey, 1e9)
+    );
+
+    const ix = await program.createBetProofInstruction({
+      house: houseAddress,
+      user: user.publicKey,
+      betValue: new anchor.BN(0.5e9),
+    });
+
+    await send(provider, [user], ix);
+
+    const betProof = findBetProofAddress({
+      user: user.publicKey,
+      house: houseAddress,
+    });
+
+    const betProofAccount = await BetProof.fetch(connection, betProof);
+
+    expect(betProofAccount.result.kind).to.equal("Unset");
+  });
+
+  it.skip("should be able to set a bet's result", async () => {
     throw "Unimplemented";
   });
 
-  it("should be able to set a bet's result", async () => {
-    throw "Unimplemented";
-  });
-
-  it("should be able to claim a bet", async () => {
+  it.skip("should be able to claim a bet", async () => {
     throw "Unimplemented";
   });
 });
