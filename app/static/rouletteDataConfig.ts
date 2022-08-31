dd20fe18b3ae3f2d1813aff78127814902d6264f
r1ck12 <rickregien@gmail.com>
2022-08-30 22:05:18 -0300

    refactor: move data config array to separate file

---
+++ app/static/rouletteDataConfig.ts
@@ -0,0 +1,98 @@
+export const data = [
+  {
+    id: 0,
+    option: '0',
+    style: {
+      backgroundColor: '#E71A1A',
+      textColor: '#fff',
+    },
+  },
+  {
+    id: 1,
+    option: '⟳',
+    style: {
+      backgroundColor: '#1A1A1A',
+      textColor: '#fff',
+    },
+  },
+  {
+    id: 2,
+    option: '2X',
+    style: {
+      backgroundColor: '#E71A1A',
+      textColor: '#fff',
+    },
+  },
+  {
+    id: 3,
+    option: '0',
+    style: {
+      backgroundColor: '#1A1A1A',
+      textColor: '#fff',
+    },
+  },
+  {
+    id: 4,
+    option: '⟳',
+    style: {
+      backgroundColor: '#E71A1A',
+      textColor: '#fff',
+    },
+  },
+  {
+    id: 5,
+    option: '2X',
+    style: {
+      backgroundColor: '#1A1A1A',
+      textColor: '#fff',
+    },
+  },
+  {
+    id: 6,
+    option: '0',
+    style: {
+      backgroundColor: '#E71A1A',
+      textColor: '#fff',
+    },
+  },
+  {
+    id: 7,
+    option: '3X',
+    style: {
+      backgroundColor: '#D1AD6B',
+      textColor: '#fff',
+    },
+  },
+  {
+    id: 8,
+    option: '0',
+    style: {
+      backgroundColor: '#E71A1A',
+      textColor: '#fff',
+    },
+  },
+  {
+    id: 9,
+    option: '2X',
+    style: {
+      backgroundColor: '#1A1A1A',
+      textColor: '#fff',
+    },
+  },
+  {
+    id: 10,
+    option: '0',
+    style: {
+      backgroundColor: '#E71A1A',
+      textColor: '#fff',
+    },
+  },
+  {
+    id: 11,
+    option: '⟳',
+    style: {
+      backgroundColor: '#1A1A1A',
+      textColor: '#fff',
+    },
+  },
+]
